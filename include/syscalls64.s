7bc4b20217676d2097704a5f2c5eaf98b629d1d9
prole <prole@thoughtgang.org>
2013-03-11 04:16:42 -0400

    Breaking out the system call %defines into the /include directory. This directory can also be used for other shared constants.

---
+++ include/syscalls64.s
@@ -0,0 +1,12 @@
+	;; Evan Jensen 64bit Put a file in /tmp and execve it shellcode
+	;; Paolo Soto
+	;; Sun Mar 10 21:37:35 EDT 2013
+
+	;; %eax = syscall number
+	;; args = %ebx, %ecx, %edx, %esi, %edi, %ebp (last can be ptr to > 6)
+	;; args = rdi, rsi, rdx, rcx, r8, r9, then stack
+	
+	%define __NR_open   BYTE 0x2
+	%define __NR_write  BYTE 0x1
+	%define __NR_mmap   BYTE 0x9
+	%define __NR_execve BYTE 0x59
