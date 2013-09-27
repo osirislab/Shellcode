1519b67e9ecf2378c7c790055ab501bcdb2f533c
Kevin Chung <kchung@nyu.edu>
2013-09-27 17:25:34 -0400

    Adding a brute function (copy of itertools.permutations()), fixing the pattern_create function in a poor manner, and repairing the pattern_offset typo.

--- isis/isis.py
+++ isis/isis.py
@@ -177,7 +177,7 @@ def pattern_create(n):
             ret = ret + i 
         else: 
             break 
-    return ret 
+    return ret[:n] 
 
 MAX_PAT=''.join(gen_pattern_string())
 
@@ -190,9 +190,33 @@ def pattern_offset(offset):
     if(type(offset)==type(999)):
         offset=hex(offset)[2:].zfill(8)
     findMe=reduce(lambda a,b:b+a,chunk(offset,2)).decode('hex')
-    return maxPat.index(findMe)
+    return MAX_PAT.index(findMe)
 
-
+def brute(iterable, r=None):
+    ''' http://docs.python.org/2/library/itertools.html#itertools.permutations '''
+    # permutations('ABCD', 2) --> AB AC AD BA BC BD CA CB CD DA DB DC
+    # permutations(range(3)) --> 012 021 102 120 201 210
+    pool = tuple(iterable)
+    n = len(pool)
+    r = n if r is None else r
+    if r > n:
+        return
+    indices = range(n)
+    cycles = range(n, n-r, -1)
+    yield tuple(pool[i] for i in indices[:r])
+    while n:
+        for i in reversed(range(r)):
+            cycles[i] -= 1
+            if cycles[i] == 0:
+                indices[i:] = indices[i+1:] + indices[i:i+1]
+                cycles[i] = n - i
+            else:
+                j = cycles[i]
+                indices[i], indices[-j] = indices[-j], indices[i]
+                yield tuple(pool[i] for i in indices[:r])
+                break
+        else:
+            return
 
 def telnet_shell(sock):
     '''pass to this function a socket object with a listening shell(socket reuse)'''
