296018d7ea4c64e9563498a032ae6f083c829270
Evan Jensen <evan.p.jensen@gmail.com>
2013-10-20 16:35:20 +0000

    there was some strange 3 constant in the remapp loop. I removed it and the program no longer pulls nulls out of the GOT

--- loader_research/mapper.c
+++ loader_research/mapper.c
@@ -214,7 +214,7 @@ int main() {
 
     //Do some remapping
     for(size_t i = 0; i < got_sz; ++i) {
-        void** addr = base + got_plt->sh_addr + (3 + i) * sizeof(void*);
+      void** addr = base + got_plt->sh_addr + (/*3 +*/ i) * sizeof(void*);
         *addr = (size_t)(*addr)+ (size_t)base;
     }
 
@@ -225,7 +225,7 @@ int main() {
     void (*init)() = base + ehdr->e_entry;
     void (*f)() = base + 0x22a86; //ehdr->e_entry + 0xbc;
 
-    raise(SIGINT);
+    //raise(SIGINT);
 
     //ep();
     printf("EP OK\n");
