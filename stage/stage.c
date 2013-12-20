1358b3cf51fdf555a9c3f9467007eed44ebba6fc
Evan Jensen <evan.p.jensen@gmail.com>
2013-12-20 16:50:25 -0500

    cleanup

---
+++ stage/stage.c
@@ -0,0 +1,112 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <sys/select.h>
+#include <stdbool.h>
+
+#include "runtime/gs.h"
+
+#define BREAK() __asm__("int3");
+
+#ifdef start
+void _start(void){
+  main();
+}
+#endif
+
+int test_functions(int,char**,char**);
+void do_child(char* command);
+void print_arg_env(char**,char**);
+void fork_and_communicate(void);
+
+int main(int argc,char** argv,char** envp){
+
+#ifdef DEBUG  
+  
+  test_functions(argc, argv, envp);
+  _exit(0);
+#endif
+  do_patch_pie();
+  
+  
+  start_main_wrapper_alt(test_functions);
+  
+}
+
+
+int test_functions(int argc,char** argv,char** envp){
+
+  
+  print_arg_env(argv,envp);
+  system("/bin/sh");
+  while(true){
+    fork_and_communicate();
+  }
+    
+  
+  _exit(0);
+  return 0;//main 2
+}
+
+void do_child(char* command){
+  int res=__libc_system(command);
+  //free(command);
+  _exit(0);
+}
+
+void print_arg_env(char** argv,char** envp){
+  _IO_puts("----------ARGS----------");
+  while(*argv){
+    _IO_puts(*(argv++));
+  }
+  _IO_puts("----------ENV----------");
+  while(*envp){
+    _IO_puts(*(envp++));
+  }
+  fflush(NULL);
+  return;
+}
+
+
+void fork_and_communicate(void){
+  
+  int pipe_fd[2];
+  __pipe(pipe_fd);
+  int read_fd=pipe_fd[0], write_fd=pipe_fd[1]; 
+ 
+  fd_set select_r_fds;
+  fd_set select_w_fds;
+  fd_set select_x_fds;
+  int max_fd=0;
+  FD_ZERO(&select_r_fds);
+  FD_ZERO(&select_w_fds);
+  FD_ZERO(&select_x_fds);
+  
+  pid_t child;
+
+  char command[0x1000];
+  memset(command,0,sizeof(command));
+  gets(command);
+
+  if((child=__fork())<0){
+    puts("There was a problem forking");
+    fflush(NULL);
+  }
+  else{
+    if(child==0){
+      dup2(read_fd,  STDIN_FILENO);
+      dup2(write_fd, STDOUT_FILENO);
+      do_child(command);
+      //child exits
+    }
+    else{
+      wait(child);
+      __close(read_fd);
+      __close(write_fd);
+      
+    }
+  }
+  return;
+}
--- stagerTest/stage/lolStage.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int main(){
-  for(int i=0;i<10;i++){
-    printf("motherfucking success, bitch!\n");
-  }
-  return 0;
-}
