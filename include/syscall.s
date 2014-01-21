8a88e1ca23345593ea042d8c2be52553b881263b
Evan Jensen <evan.p.jensen@gmail.com>
2014-01-20 21:12:37 -0500

    new syscall macros

--- include/syscall.s
+++ include/syscall.s
@@ -1,6 +1,7 @@
-	;; define the SYSTEM_CALL macro
-
-
+;;; Define the SYSTEM_CALL macro
+;;; YOU MUST USE A %DEFINE IF YOU USE THIS MACRO
+;;; Will emit a hlt instruction if no %define is present
+	
 %macro SYSTEM_CALL 0
 	%ifdef INT80
 	int 0x80
@@ -11,6 +12,30 @@
 	%elifdef CALLGATE32
 	call [gs:eiz+0x10]
 	%else
-	hlt			;YOU MUST USE A %DEFINE IF YOU USE THIS MACRO
+	hlt			
+	%endif
+%endmacro
+
+%macro SYSTEM_CALL 1
+	
+	%ifdef INT80
+	xor eax,eax;used most frequently with 32bit shellcode
+	mov al, %1
+	SYSTEM_CALL
+	
+	%elifdef SYSENTER
+	push byte %1
+	pop rax			;used most frequently with 64bit shellcode
+	SYSTEM_CALL
+	
+	%elifdef SYSCALL
+	int3
+	hlt
+	%elifdef CALLGATE32	;32bit only
+	xor eax,eax
+	mov al, %1
+	SYSTEM_CALL 
+	%else
+	hlt	
 	%endif
 %endmacro
