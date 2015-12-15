77694f9a7987acc34afe5cd34be78c2b0d07b557
Shark64 <shark@bitchx.it>
2015-12-15 22:32:18 +0100

    shorter opcodes

--- 64shellEmulator/shell64.s
+++ 64shellEmulator/shell64.s
@@ -11,47 +11,45 @@
 main:
 
 	do_fork:
-	push byte fork
-	pop rax
-	SYSTEM_CALL
-	test rax,rax
+	xor eax, eax
+	mov al fork
+	syscall
+	test eax,eax
 	jz child
 parent:
-	push wait4
-	pop rax
-	xor rdi,rdi		;pid
-	xor rsi,rsi		;status
-	xor rdx,rdx		;options
-	xor rcx,rcx		;struct rusage*=NULL
-	SYSTEM_CALL		;wait(0,0,0,0);
+	xor edi,edi		;pid
+	xor esi,esi		;status
+	xor edx,edx		;options
+	xor ecx,ecx		;struct rusage*=NULL
+	lea eax,[rdi+wait4]
+	syscall			;wait(0,0,0,0);
 	jmp main
 
 child:	
 	cld
 get_input:
-	xor rax,rax
-	cdq
-	mov dx,BUFFERLEN        ;size of read
+	xor eax,eax
+	lea edx,[rax+BUFFERLEN] ;size of read
+	mov r8d, edx		;save readsize
 	sub rsp,rdx		;make some room on the stack
 	mov rsi,rsp		;use new stack space as buffer for read
-	xor rdi,rdi		;fd
+	xor edi,edi		;fd
 	mov al,read	
-	SYSTEM_CALL		;read into stack buffer
+	syscall			;read into stack buffer
 	
-	mov rbp,rax		;save len of str_read
-	test rax,rax		;we must read more than 0 bytes
+	mov ebp,eax		;save len of str_read
+	test eax,eax		;we must read more than 0 bytes
 	jz do_exit 		;synchronous IO or GTFO
-	mov byte [rax+rsp-1],0	;replace newline with nullbyte
-	push rax		;save strlen on the stack
+	xor edx, edx
+	lea rcx, [rax+rsp]
+	mov byte [rcx-1],dl	;replace newline with nullbyte
 	
-
 	;; let's parse the arguments here
-	pop rcx			;return of read pushed by get_input
-	push byte " "		;delimiter
-	pop rax			;we're going to inline a strchr
+	mov ecx,eax		;return of read pushed by get_input
+	lea eax, [rdx+0x20]	;" " (space) is the delimiter
 	mov rbx,rsp		;rbx is the buffer
-	xor rdx,rdx
-	add rsp,BUFFERLEN	;rsp is now going to be argv
+	xor edx,edx
+	add rsp,r8		;rsp is now going to be argv
 add_token: 	;; calculate the pointer to push
 	
 	mov rsi,rbp		;number of chars in buffer
@@ -60,33 +58,28 @@ add_token: 	;; calculate the pointer to push
  	
 	lea rdi,[rbx + rsi]  	;rdi points to current token
 	mov [rsp+rdx*8], rdi 	;save the current token pointer building argv
-	inc rdx			;increment index into argv
-	
-	
+	inc edx			;increment index into argv
+scan_loop:	
 	repne scasb
 	
 	mov rsi,rbp
 	sub rsi,rcx
-	mov byte[rbx+rsi-1],0	;null terminate each token (strtok)
+	xor eax, eax
+	mov byte[rbx+rsi-1],al	;null terminate each token (strtok)
 	
 	test rcx,rcx
-	jz exec
-	
-	jmp short add_token
+	jnz short add_token
 	
 exec:
-	xor rax,rax
+	xor eax,eax
 	mov [rsp+rdx*8],rax
-	cdq
-
 	mov al,execve
 	mov rdi,rbx
 	mov rsi,rsp
-	;; rdx=null
-	SYSTEM_CALL		;execve(cmd,args,environ=NULL);
+	xor edx, edx		; rdx=null
+	syscall		;execve(cmd,args,environ=NULL);
 	
 do_exit:;; exit nicely if anything fails
-	push byte exit
-	pop rax
-	xor rdi,rdi
-	SYSTEM_CALL
+	xor edi,edi
+	lea eax,[rdi+exit]
+	syscall
